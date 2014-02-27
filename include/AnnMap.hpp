dd700b5a101650b417b2d7548e879664bf7460e5
Ybalrid <ybalrid@cecmu.org>
2014-02-27 15:50:23 +0000

    Ann AnnMap class source files

---
+++ include/AnnMap.hpp
@@ -0,0 +1,47 @@
+#ifndef ANN_MAP
+#define ANN_MAP
+
+#undef DLL
+//windows DLL
+#ifdef DLLDIR_EX
+#define DLL  __declspec(dllexport)   // export DLL information
+#else
+#define DLL  __declspec(dllimport)   // import DLL information
+#endif
+
+//bypass on linux
+#ifdef __gnu_linux__
+#undef DLL
+#define DLL
+#endif
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "AnnEngine.hpp"
+#include "AnnGameObject.hpp"
+#include "AnnTypes.h"
+namespace Annwvyn
+{
+    class DLL AnnMap
+    {
+        public:
+            AnnMap(Annwvyn::AnnEngine* engine);
+            AnnMap(Annwvyn::AnnEngine* engine, const char mapFile[]);
+            ~AnnMap();
+            void loadFile(const char mapFile[]);
+       
+        private:
+            void init(Annwvyn::AnnEngine* engine);
+            void process(std::string descLine);
+
+        private:
+            AnnEngine* m_engine;
+            AnnGameObjectVect content;
+            bool fileLoaded;
+
+            Annwvyn::AnnGameObect* tmpObject;
+    };
+}
+#endif //ANN_MAP 
+
