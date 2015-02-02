c654c24ba0f36a2decadb37c5f774704da5c20f7
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2015-02-02 14:18:58 +0100

    Start building interface to access player's data securely

--- src/AnnPlayer.cpp
+++ src/AnnPlayer.cpp
@@ -17,6 +17,7 @@ bodyParams::bodyParams()
 AnnPlayer::AnnPlayer()
 {
 	playerBody = new bodyParams;
+	locked = false;
 }
 
 AnnPlayer::~AnnPlayer()
@@ -24,7 +25,117 @@ AnnPlayer::~AnnPlayer()
 	delete playerBody;
 }
 
+bool AnnPlayer::isLocked()
+{
+	return locked;
+}
+
+
+void AnnPlayer::setPosition(Ogre::Vector3 Position)
+{
+		playerBody->Position = Position;
+}
+
+
+void AnnPlayer::setOrientation(Ogre::Euler Orientation)
+{
+		playerBody->Orientation = Orientation;
+}
+
+void AnnPlayer::setHeadOrientation(Ogre::Quaternion Orientation)
+{
+		playerBody->HeadOrientation = Orientation;
+}
+
+void AnnPlayer::setEyesHeight(float eyeHeight)
+{
+	if(!isLocked())
+		playerBody->eyeHeight = eyeHeight;
+}
+
+
+void AnnPlayer::setWalkSpeed(float walk)
+{
+	if(!isLocked())
+		playerBody->eyeHeight = walk;
+}
+
+void AnnPlayer::setTurnSpeed(float ts)
+{
+	if(!isLocked())
+		playerBody->eyeHeight = ts;
+}
+
+void AnnPlayer::setMass(float mass)
+{
+	if(!isLocked())
+		playerBody->eyeHeight = mass;
+}
+
+void AnnPlayer::setShape(btCollisionShape* Shape)
+{
+	if(!isLocked())
+		playerBody->Shape = Shape;
+}
+
+void AnnPlayer::setBody(btRigidBody* Body)
+{
+	if(!isLocked())
+		playerBody->Body = Body;
+}
+
+
+
+
+void AnnPlayer::lockParameters()
+{
+	locked = true;
+}
+
+void AnnPlayer::unlockParameters()
+{
+	locked = true;
+}
+
 bodyParams* AnnPlayer::getLowLevelBodyParams()
 {
 	return playerBody;
+}
+
+
+float AnnPlayer::getWalkSpeed()
+{
+	return playerBody->walkSpeed;
+}
+
+float AnnPlayer::getEyesHeight()
+{
+	return playerBody->eyeHeight;
+}
+
+float AnnPlayer::getTurnSpeed()
+{
+	return playerBody->turnSpeed;
+}
+
+float AnnPlayer::getMass()
+{
+	return playerBody->mass;
+}
+
+
+btRigidBody* AnnPlayer::getBody()
+{
+	return playerBody->Body;
+}
+
+
+btCollisionShape* AnnPlayer::getShape()
+{
+	return playerBody->Shape;
+}
+
+Ogre::Vector3 AnnPlayer::getPosition()
+{
+	return playerBody->Position;
 }
