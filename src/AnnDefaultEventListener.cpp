0f3a74dabdfa6ef6c52128d4997b3666246891de
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2015-02-06 11:43:34 +0100

    start implementing a rudimentary event management system

---
+++ src/AnnDefaultEventListener.cpp
@@ -0,0 +1,15 @@
+#include "AnnEventManager.hpp"
+
+using namespace Annwvyn;
+
+void AnnDefaultEventListener::KeyEvent(AnnKeyEvent e)
+{
+}
+
+void AnnDefaultEventListener::MouseEvent(AnnMouseEvent e)
+{
+}
+
+void AnnDefaultEventListener::StickEvent(AnnStickEvent e)
+{
+}
---
+++ src/AnnEventManager.cpp
@@ -0,0 +1,11 @@
+#include "AnnEventManager.hpp"
+
+using namespace Annwvyn;
+
+AnnEventManager::AnnEventManager()
+{
+}
+
+AnnEventManager::~AnnEventManager()
+{
+}
