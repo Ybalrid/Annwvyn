c772cdf446e982b08cb7d3f1023cf5e459cda746
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2015-02-15 14:44:28 +0100

    remove old commented code

--- src/AnnTools.cpp
+++ src/AnnTools.cpp
@@ -6,20 +6,6 @@ using namespace Ogre;
 
 float Annwvyn::Tools::Geometry::distance(Annwvyn::AnnGameObject* a, Annwvyn::AnnGameObject* b)
 {
-/*	Vector3 vector(Vector3(0,0,0));
-
-	vector += (b->pos().x - a->pos().x);
-	vector += (b->pos().y - a->pos().y);
-	vector += (b->pos().z - a->pos().z);
-
-	float dist;
-
-	dist = vector.x * vector.x;
-	dist += vector.y * vector.y;
-	dist += vector.z * vector.z;
-
-	return sqrt(dist)*/
-
     return Annwvyn::Tools::Geometry::distance(a->pos(),b->pos());
 }
 
