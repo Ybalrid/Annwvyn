946d961e615abee0914e8c65685b110fa1109a6d
Ybalrid <ybalrid@cecmu.org>
2015-04-02 19:23:05 +0200

    Change the doxygen commit style in Euler.h

--- include/euler.h
+++ include/euler.h
@@ -1,10 +1,10 @@
 /**
-@file Euler.h
-@brief %Euler class for %Ogre
-@details License: Do whatever you want with it.
-@version 2.1
-@author Kojack
-@author Transporter
+\file Euler.h
+\brief %Euler class for %Ogre
+\details License: Do whatever you want with it.
+\version 2.1
+\author Kojack
+\author Transporter
 */
 #ifndef OGREEULER_H
 #define OGREEULER_H
@@ -18,8 +18,8 @@ namespace Ogre
 {
  
 /**
-@class Euler
-@brief Class for %Euler rotations
+\class Euler
+\brief Class for %Euler rotations
  
 <table><tr><td>Yaw is a rotation around the Y axis.</td><td>Pitch is a rotation around the X axis.</td><td>Roll is a rotation around the Z axis.</td></tr>
 <tr><td><img src="http://www.ogre3d.org/tikiwiki/tiki-download_file.php?fileId=2112" /></td><td><img src="http://www.ogre3d.org/tikiwiki/tiki-download_file.php?fileId=2113" /></td><td><img src="http://www.ogre3d.org/tikiwiki/tiki-download_file.php?fileId=2114" /></td></tr></table>
@@ -34,10 +34,10 @@ public:
 	}
  
 	/**
-	@brief Constructor which takes yaw, pitch and roll values.	
-	@param y Starting value for yaw
-	@param p Starting value for pitch
-	@param r Starting value for roll
+	\brief Constructor which takes yaw, pitch and roll values.	
+	\param y Starting value for yaw
+	\param p Starting value for pitch
+	\param r Starting value for roll
 	*/
 	Euler(Ogre::Radian y, Ogre::Radian p = Ogre::Radian(0.0f), Ogre::Radian r = Ogre::Radian(0.0f))
 		: mYaw(y), mPitch(p), mRoll(r), mChanged(true) 
@@ -45,10 +45,10 @@ public:
 	}
  
 	/**
-	@brief Constructor which takes yaw, pitch and roll values as ints (degrees).
-	@param y Starting value for yaw [degree]
-	@param p Starting value for pitch [degree]
-	@param r Starting value for roll [degree]
+	\brief Constructor which takes yaw, pitch and roll values as ints (degrees).
+	\param y Starting value for yaw [degree]
+	\param p Starting value for pitch [degree]
+	\param r Starting value for roll [degree]
 	*/
 	Euler(int y, int p = 0, int r = 0)
 		: mYaw(Ogre::Degree((Ogre::Real)y)), mPitch(Ogre::Degree((Ogre::Real)p)), mRoll(Ogre::Degree((Ogre::Real)r)), mChanged(true) 
@@ -56,10 +56,10 @@ public:
 	}
  
  	/**
-	@brief Constructor which takes yaw, pitch and roll values as reals (radians).
-	@param y Starting value for yaw [radian]
-	@param p Starting value for pitch [radian]
-	@param r Starting value for roll [radian]
+	\brief Constructor which takes yaw, pitch and roll values as reals (radians).
+	\param y Starting value for yaw [radian]
+	\param p Starting value for pitch [radian]
+	\param r Starting value for roll [radian]
 	*/
 	Euler(Ogre::Real y, Ogre::Real p = 0.0f, Ogre::Real r = 0.0f)
 		: mYaw(Ogre::Radian(y)), mPitch(Ogre::Radian(p)), mRoll(Ogre::Radian(r)), mChanged(true) 
@@ -67,8 +67,8 @@ public:
 	}
  
 	/**
-	@brief Default constructor with presets.
-	@param axvec Starting values as a combined vector
+	\brief Default constructor with presets.
+	\param axvec Starting values as a combined vector
 	*/
 	Euler(Ogre::Vector3 axvec)
 		: mYaw(Ogre::Radian(axvec.y)), mPitch(Ogre::Radian(axvec.x)), mRoll(Ogre::Radian(axvec.z)), mChanged(true) 
@@ -76,8 +76,8 @@ public:
 	}
  
 	/**
-	@brief Default constructor with presets.
-	@param quaternion Calculate starting values from this quaternion
+	\brief Default constructor with presets.
+	\param quaternion Calculate starting values from this quaternion
 	*/
 	explicit Euler(Ogre::Quaternion quaternion) 
 	{
@@ -94,8 +94,8 @@ public:
 	inline Ogre::Radian getRoll() { return mRoll; }
  
 	/**
-	@brief Set the yaw.
-	@param y New value for yaw
+	\brief Set the yaw.
+	\param y New value for yaw
 	*/
 	inline Euler &setYaw(Ogre::Radian y)
 	{
@@ -105,8 +105,8 @@ public:
 	}
  
 	/**
-	@brief Set the pitch.
-	@param p New value for pitch
+	\brief Set the pitch.
+	\param p New value for pitch
 	*/
 	inline Euler &setPitch(Ogre::Radian p) 
 	{
@@ -116,8 +116,8 @@ public:
 	}
  
 	/**
-	@brief Set the roll.
-	@param r New value for roll
+	\brief Set the roll.
+	\param r New value for roll
 	*/
 	inline Euler &setRoll(Ogre::Radian r) 
 	{
@@ -127,10 +127,10 @@ public:
 	}
  
 	/**
-	@brief Set all rotations at once.
-	@param y New value for yaw
-	@param p New value for pitch
-	@param r New value for roll
+	\brief Set all rotations at once.
+	\param y New value for yaw
+	\param p New value for pitch
+	\param r New value for roll
 	*/
 	inline Euler &setRotation(Ogre::Radian y, Ogre::Radian p, Ogre::Radian r)
 	{
@@ -142,8 +142,8 @@ public:
 	}
  
 	/**
-	@brief Apply a relative yaw.
-	@param y Angle to add on current yaw
+	\brief Apply a relative yaw.
+	\param y Angle to add on current yaw
 	*/
 	inline Euler &yaw(Ogre::Radian y) 
 	{
@@ -153,8 +153,8 @@ public:
 	}
  
 	/**
-	@brief Apply a relative pitch.
-	@param p Angle to add on current pitch
+	\brief Apply a relative pitch.
+	\param p Angle to add on current pitch
 	*/
 	inline Euler &pitch(Ogre::Radian p) 
 	{
@@ -164,8 +164,8 @@ public:
 	}
  
 	/**
-	@brief Apply a relative roll.
-	@param r Angle to add on current roll
+	\brief Apply a relative roll.
+	\param r Angle to add on current roll
 	*/
 	inline Euler &roll(Ogre::Radian r) 
 	{
@@ -175,10 +175,10 @@ public:
 	}
  
 	/**
-	@brief Apply all relative rotations at once.
-	@param y Angle to add on current yaw
-	@param p Angle to add on current pitch
-	@param r Angle to add on current roll
+	\brief Apply all relative rotations at once.
+	\param y Angle to add on current yaw
+	\param p Angle to add on current pitch
+	\param r Angle to add on current roll
 	*/
 	inline Euler &rotate(Ogre::Radian y, Ogre::Radian p, Ogre::Radian r)
 	{
@@ -199,8 +199,8 @@ public:
 	inline Ogre::Vector3 getUp() { return toQuaternion() * Ogre::Vector3::UNIT_Y; }
  
 	/**
-	@brief Calculate the quaternion of the euler object.
-	@details The result is cached, it is only recalculated when the component euler angles are changed.
+	\brief Calculate the quaternion of the euler object.
+	\details The result is cached, it is only recalculated when the component euler angles are changed.
 	*/
 	inline Ogre::Quaternion toQuaternion() 
 	{
@@ -219,8 +219,8 @@ public:
 	}
  
 	/**
-	@brief Calculate the current eulers of a given quaternion object.
-	@param quaternion Quaternion which is used to calculate current euler angles
+	\brief Calculate the current eulers of a given quaternion object.
+	\param quaternion Quaternion which is used to calculate current euler angles
 	*/
 	inline void fromQuaternion(Ogre::Quaternion quaternion)
 	{
@@ -244,10 +244,10 @@ public:
 	}
  
 	/**
-	@brief Set the yaw and pitch to face in the given direction.
-	@details The direction doesn't need to be normalised. Roll is always unaffected.
-	@param setYaw If false, the yaw isn't changed.
-	@param setPitch If false, the pitch isn't changed.
+	\brief Set the yaw and pitch to face in the given direction.
+	\details The direction doesn't need to be normalised. Roll is always unaffected.
+	\param setYaw If false, the yaw isn't changed.
+	\param setPitch If false, the pitch isn't changed.
 	*/
 	inline Euler &setDirection(const Ogre::Vector3 &v, bool setYaw = true, bool setPitch = true)
 	{
@@ -262,11 +262,11 @@ public:
 	}
  
 	/**
-	@brief Normalise the selected rotations to be within the +/-180 degree range.
-	@details The normalise uses a wrap around, so for example a yaw of 360 degrees becomes 0 degrees, and -190 degrees becomes 170.
-	@param normYaw If false, the yaw isn't normalized.
-	@param normPitch If false, the pitch isn't normalized.
-	@param normRoll If false, the roll isn't normalized.
+	\brief Normalise the selected rotations to be within the +/-180 degree range.
+	\details The normalise uses a wrap around, so for example a yaw of 360 degrees becomes 0 degrees, and -190 degrees becomes 170.
+	\param normYaw If false, the yaw isn't normalized.
+	\param normPitch If false, the pitch isn't normalized.
+	\param normRoll If false, the roll isn't normalized.
 	*/
 	inline void normalise(bool normYaw = true, bool normPitch = true, bool normRoll = true)
 	{
@@ -345,12 +345,12 @@ public:
 	}
  
 	/**
-	@brief Return the relative euler angles required to rotate from the current forward direction to the specified dir vector.
-	@details The result euler can then be added to the current euler to immediately face dir.
+	\brief Return the relative euler angles required to rotate from the current forward direction to the specified dir vector.
+	\details The result euler can then be added to the current euler to immediately face dir.
 	The rotation won't flip upside down then roll instead of a 180 degree yaw.
-	@param setYaw If false, the angle is set to 0. If true, the angle is calculated.
-	@param setPitch If false, the angle is set to 0. If true, the angle is calculated.
-	@param shortest If false, the full value of each angle is used. If true, the angles are normalised and the shortest
+	\param setYaw If false, the angle is set to 0. If true, the angle is calculated.
+	\param setPitch If false, the angle is set to 0. If true, the angle is calculated.
+	\param shortest If false, the full value of each angle is used. If true, the angles are normalised and the shortest
 	rotation is found to face the correct direction. For example, when false a yaw of 1000 degrees and a dir of
 	(0,0,-1) will return a -1000 degree yaw. When true, the same yaw and dir would give 80 degrees (1080 degrees faces
 	the same way as (0,0,-1).
@@ -430,8 +430,8 @@ public:
 	inline Euler operator+(const Ogre::Vector3 &angles) const { return Euler(mYaw + Ogre::Radian(angles.y), mPitch + Ogre::Radian(angles.x), mRoll + Ogre::Radian(angles.z)); }
  
 	/**
-	@brief Subtract two euler objects.
-	@details This finds the difference as relative angles.
+	\brief Subtract two euler objects.
+	\details This finds the difference as relative angles.
 	*/
  	inline Euler operator-(const Euler &rhs) const { return Euler(mYaw - rhs.mYaw, mPitch - rhs.mPitch, mRoll - rhs.mRoll); }
  
@@ -445,9 +445,9 @@ public:
 	inline friend Euler operator*(Ogre::Real lhs, const Euler &rhs) { return Euler(lhs * rhs.mYaw, lhs * rhs.mPitch, lhs * rhs.mRoll); }
  
 	/**
-	@brief Multiply two eulers.
-	@details This has the same effect as multiplying quaternions.
-	@returns The result is a quaternion.
+	\brief Multiply two eulers.
+	\details This has the same effect as multiplying quaternions.
+	\returns The result is a quaternion.
 	*/
 	inline Ogre::Quaternion operator*(const Euler &rhs) const
 	{
