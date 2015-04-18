19c4d4f53e3139d35aee909f5eca30086f0feec0
Taiki <Taiki-San@users.noreply.github.com>
2015-04-18 23:09:10 +0200

    Remove duplicated code

--- src/AnnDefaultEventListener.cpp
+++ src/AnnDefaultEventListener.cpp
@@ -44,38 +44,20 @@ void AnnDefaultEventListener::setKeys(KeyCode::code fw,
 //It will normally switch the keyboard back to it's original configuration once the AnnEngine object is destroyed
 void AnnDefaultEventListener::KeyEvent(AnnKeyEvent e)
 {
-	//TODO having a pointer to the AnnPlayer object would be nice...
-	if(e.isPressed())
-	{
-		//modify linear speed vector
-		if(e.getKey() == forward)
-			player->walking[walkDirection::forward] = true;
-		if(e.getKey() == backward)
-			player->walking[walkDirection::backward] = true;
-		if(e.getKey() == straffleft)
-			player->walking[walkDirection::left] = true;
-		if(e.getKey() == straffright)
-			player->walking[walkDirection::right] = true;
-        if(e.getKey() == jump)
-            player->jump();
-        if(e.getKey() == run)
-            player->run = true;
-	}
+	if(e.getKey() == forward)
+		player->walking[walkDirection::forward] = e.isPressed();
+	if(e.getKey() == backward)
+		player->walking[walkDirection::backward] = e.isPressed();
+	if(e.getKey() == straffleft)
+		player->walking[walkDirection::left] = e.isPressed();
+	if(e.getKey() == straffright)
+		player->walking[walkDirection::right] = e.isPressed();
+	if(e.getKey() == run)
+		player->run = e.isPressed();
 
-	else if(e.isRelased())
-	{
-		//modify linear speed vector
-		if(e.getKey() == forward)
-			player->walking[walkDirection::forward] = false;
-		if(e.getKey() == backward)
-			player->walking[walkDirection::backward] = false;
-		if(e.getKey() == straffleft)
-			player->walking[walkDirection::left] = false;
-		if(e.getKey() == straffright)
-			player->walking[walkDirection::right] = false;
-        if(e.getKey() == run)
-            player->run = false;
-	}
+	//Special case
+	if(e.isPressed() && e.getKey() == jump)
+		player->jump();
 }
 
 //The mouse event contain all information about the mouse. Mouse mouvement are integer and are represented by 3 axis 
