c50c64cbd694f942baeb9640cb1243ecb55d3d47
Ybalrid <ybalrid@cecmu.org>
2015-05-07 23:53:01 +0200

    Update doxygen's main page

--- include/Annwvyn.h
+++ include/Annwvyn.h
@@ -13,21 +13,41 @@
  *\mainpage Annwvyn Game Engine API Reference documentation
  *
  * Welcome to the Annwvyn API documentation.
- *
- * Here you'll find the complete & exhaustive documentation of the engine (last version avilable in the master branch at the moment this site has been generated).
- *
- * I strongly recomned you to go to <a href="http://wiki.annwvyn.org/">the wiki</a> to get a more comprehensible help to start using the engine. There is a quick-start tutorial you can check-out.
- * The engine is based on Ogre and take for reference Ogre's resource management, file format, datastructures and geometric classes. 
- *
- * The aim of the project is to create something pretty strait-forward to make 3D interactive content. Making an high-level framework to create Oculus Rift compatible content. You dont't have to worry about doing the rendering, the tracking, simulating basic physics, ect.
  * 
- * If you have any suggestion, feel free to email me at <a href="mailto:developer@annwvyn.org">developer@annwvyn.org</a>.
+ * Annwvyn is a free and Open-Soucre Virtual reality developement framework
+ * <em>The project is under cosntant developement and there isn't yet any stable
+ * release available. It comes without any warenty and is distributed under the
+ * terms of the MIT Licence agreement. Full copy of the licence is available on the 
+ * source code repository</em>
+ *
+ * <em>Also, please note that, because of the on-going developement nature of this 
+ * project, any part of the presented API may change without any notice.</em>
+ *
+ * Here you'll find the complete & exhaustive documentation of the engine 
+ * (last version avilable in the master branch at the date this site has been 
+ * generated).
+ *
+ * I strongly recomned you to go to <a href="http://wiki.annwvyn.org/">the wiki</a>
+ * to get a more comprehensible help to start using the engine, 
+ * there is a quick-start tutorial you can check-out and more contend will be added 
+ * regulary
+ * The engine is based on Ogre and take for reference Ogre's resource management, 
+ * file format, datastructures and geometric classes. Everything 3D related on
+ * Annwvyn is handeled by Ogre and is only intended to be used with the GL render
+ * system. The oculus integration is partially written in GLSL wich is OpenGL shading
+ * language.
+ *
+ * The aim of the project is to making an high-level (yet really fast) framework 
+ * to create Oculus Rift compatible content. 
+ * You dont't have to worry about doing the rendering, 
+ * the tracking, simulating basic physics, ect.
+ * 
+ * If you have any suggestion, feel free to email me at 
+ * <a href="mailto:developer@annwvyn.org">developer@annwvyn.org</a>.
  *
  * Enjoy your stay and make great virtual reality experiencies ;-)
  *
  */
-
-//#include "OgreOculus.h"
 
 //Annwvyn classes
 #include "AnnEngine.hpp"
