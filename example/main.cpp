d760a6be3f9274631da689fdc7cda605466f8e55
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2015-05-17 20:45:33 +0200

    Add debug console on windows. Can be oppened with a call of AnnEngine::openConsole() (static)

--- example/main.cpp
+++ example/main.cpp
@@ -39,39 +39,12 @@ public:
 	}
 };
 
-static void OpenConsole()
-{
-    int outHandle, errHandle, inHandle;
-    FILE *outFile, *errFile, *inFile;
-    AllocConsole();
-    CONSOLE_SCREEN_BUFFER_INFO coninfo;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
-    coninfo.dwSize.Y = 9999;
-    SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
 
-    outHandle = _open_osfhandle((long)GetStdHandle(STD_OUTPUT_HANDLE), _O_TEXT);
-    errHandle = _open_osfhandle((long)GetStdHandle(STD_ERROR_HANDLE),_O_TEXT);
-    inHandle = _open_osfhandle((long)GetStdHandle(STD_INPUT_HANDLE),_O_TEXT );
-
-    outFile = _fdopen(outHandle, "w" );
-    errFile = _fdopen(errHandle, "w");
-    inFile =  _fdopen(inHandle, "r");
-
-    *stdout = *outFile;
-    *stderr = *errFile;
-    *stdin = *inFile;
-
-    setvbuf( stdout, NULL, _IONBF, 0 );
-    setvbuf( stderr, NULL, _IONBF, 0 );
-    setvbuf( stdin, NULL, _IONBF, 0 );
-
-    std::ios::sync_with_stdio();
-
-}
 AnnMain()
 {
-	OpenConsole();
 	//create Annwvyn engine
+	//Only usefull on windows : Open a debug console 
+	AnnEngine::openConsole();
 	AnnEngine* GameEngine(new AnnEngine("A Game"));
 	//load ressources
 	GameEngine->loadDir("media/dome");
