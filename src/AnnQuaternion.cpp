5cbe15191f5703a80e1203ab725aa104d2d9361c
Ybalrid <ybalrid@cecmu.org>
2015-05-21 12:07:42 +0200

    export the quaternion class to the DLL when building Annwvyn

--- src/AnnQuaternion.cpp
+++ src/AnnQuaternion.cpp
@@ -3,7 +3,7 @@
 
 using namespace Annwvyn;
 
-AnnQuaternion::AnnQuaternion(){}
+AnnQuaternion::AnnQuaternion(){}	
 AnnQuaternion::AnnQuaternion(const Ogre::Quaternion& q) : Ogre::Quaternion(q){}
 AnnQuaternion::AnnQuaternion(float cw, float cx, float cy, float cz) : Ogre::Quaternion(cw,cx,cy,cz){}
 AnnQuaternion::AnnQuaternion(const Ogre::Matrix3& rot) : Ogre::Quaternion(rot){}
