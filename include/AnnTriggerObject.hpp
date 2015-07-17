8583b7af36ac4c70af5ea19d77318efd5f8a333d
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2015-07-17 20:42:08 +0200

    Document new class AnnAlignedBoxTriggerObject

--- include/AnnTriggerObject.hpp
+++ include/AnnTriggerObject.hpp
@@ -48,6 +48,8 @@ namespace Annwvyn
 		///For engine : Set contact state 
 		/// \param contact Contact state
 		void setContactInformation(bool contact);
+		
+		///Return true if player's head (this is player's trigger point) is inside the trigger volume.
 		virtual bool computeVolumetricTest(AnnPlayer* player) = 0;
 
 		friend class AnnEngine;
@@ -85,13 +87,25 @@ namespace Annwvyn
 		float m_threshold;
 	};
 
+	///Create a trigger volume that is aligned with the scene referential.
+	///Volume is defined by min/max XYZ boundaries
+	///This is the lowest load in CPU time
 	class DLL AnnAlignedBoxTriggerObject : public AnnTriggerObject
 	{
 	public:
 		AnnAlignedBoxTriggerObject();
+
+		///Set the volume dimentions
+		/// \param x1 X minimal plane boundary
+		/// \param x2 X maximal plane boundary
+		/// \param y1 Y minimal plane boundary
+		/// \param y2 Y maximal plane boundary
+		/// \param z1 Z minimal plane boundary
+		/// \param z2 Z maximal plane boundary
 		void setBoundaries(float x1, float x2, float y1, float y2, float z1, float z2);
 	private:
 		bool computeVolumetricTest(AnnPlayer* player);
+		///Boundaries values. All defaults to 0
 		float xMin, xMax, yMin, yMax, zMin, zMax;
 	};
 }
