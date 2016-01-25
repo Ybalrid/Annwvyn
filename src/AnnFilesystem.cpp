7c568a1425ea701f61dd328bfad1062b3f0e2fe4
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2016-01-25 23:33:09 +0100

    Do the ground work for file data storage

---
+++ src/AnnFilesystem.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "AnnFilesystem.hpp"
+#include "AnnLogger.hpp"
+
+using namespace Annwvyn;
+using namespace std;
+
+AnnFilesystemManager::AnnFilesystemManager()
+{
+	AnnDebug() << "Filesystem manager created";
+
+
+}
