74f9e56b2cc6d820755618aeec7c7d1fcadc35dc
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2016-01-26 03:44:48 +0100

    Create savedir & can write stored map to textfile

--- include/AnnFilesystem.hpp
+++ include/AnnFilesystem.hpp
@@ -6,31 +6,51 @@
 #include <map>
 #include <algorithm>
 #include <vector>
+#include <fstream>
+#ifdef WIN32
+#include <Windows.h>
+#endif
 
 using namespace std;
 
 namespace Annwvyn
 {
+	class AnnSaveFileData;
+	class DLL AnnFileWriter
+	{
+	public:
+		AnnFileWriter();
+		void write(AnnSaveFileData* dataToWrite);
+	};
 	class DLL AnnFilesystemManager
 	{
 	public:
 		AnnFilesystemManager();
+		~AnnFilesystemManager();
 		void setSaveDirectoryName(string name);
 		string getPathForFileName(string fileName);
+		string getSaveDirectoryFullPath();
+		static void createDirectory(string path);
+		void createSaveDirectory();
 	private:
 		string saveDirectoryName;
 		string pathToUserDir;
 		std::vector<char> charToEscape;
+		AnnFileWriter* fileWriter;
 	};
-
+	
 	class DLL AnnSaveFileData
 	{
 	public:
 		AnnSaveFileData(string name);
 	private:
+		friend class AnnFileWriter;
 		string fileName;
+	public:
 		std::map<string, string> storedTextData; 
 	};
+
+
 }
 
 #endif
