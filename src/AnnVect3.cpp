7411e17547554e1bff1d53f6c5527a65dfc8bbd7
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2016-01-29 21:10:22 +0100

    Add valid/invalid flag to Vect3 and Quaternion

--- src/AnnVect3.cpp
+++ src/AnnVect3.cpp
@@ -3,23 +3,36 @@
 
 using namespace Annwvyn;
 
-AnnVect3::AnnVect3() : Ogre::Vector3(){}
+AnnVect3::AnnVect3() : Ogre::Vector3(){init();}
 
-AnnVect3::AnnVect3(const Ogre::Vector3& v) : Ogre::Vector3(v){}
+AnnVect3::AnnVect3(const Ogre::Vector3& v) : Ogre::Vector3(v){init();}
 
-AnnVect3::AnnVect3(const float cx, const float cy, const float cz) : Ogre::Vector3(cx, cy, cz){}
+AnnVect3::AnnVect3(const float cx, const float cy, const float cz) : Ogre::Vector3(cx, cy, cz){init();}
 
-AnnVect3::AnnVect3(const int coord[3]) : Ogre::Vector3(coord){}
+AnnVect3::AnnVect3(const int coord[3]) : Ogre::Vector3(coord){init();}
 
-AnnVect3::AnnVect3(const float coord[3]) : Ogre::Vector3(coord){}
+AnnVect3::AnnVect3(const float coord[3]) : Ogre::Vector3(coord){init();}
 
-AnnVect3::AnnVect3(float* const r) : Ogre::Vector3(r){}
+AnnVect3::AnnVect3(float* const r) : Ogre::Vector3(r){init();}
 
-AnnVect3::AnnVect3(const float s) : Ogre::Vector3(s){}
+AnnVect3::AnnVect3(const float s) : Ogre::Vector3(s){init();}
 
-AnnVect3::AnnVect3(btVector3& v) : Ogre::Vector3(v.x(), v.y(), v.z()){}
+AnnVect3::AnnVect3(btVector3& v) : Ogre::Vector3(v.x(), v.y(), v.z()){init();}
+
+AnnVect3::AnnVect3(bool validState) : Ogre::Vector3(){init(); valid = validState;}
+
+void AnnVect3::init()
+{
+	valid = true;
+}
 
 btVector3 AnnVect3::getBtVector()
 {
 	return btVector3(x, y, z);
 }
+
+bool AnnVect3::isValid()
+{
+	return valid;
+}
+
