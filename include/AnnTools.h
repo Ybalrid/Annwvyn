29b3f0e704e95ba22bbfba09c134fa6108924995
Ybalrid <ybalrid@cecmu.org>
2016-04-23 21:52:32 +0200

    Get rid of stupid "distance" function

--- include/AnnTools.h
+++ include/AnnTools.h
@@ -18,17 +18,7 @@ namespace Annwvyn
 {
 	///Return the identifier of a shape form it's name in text
 	phyShapeType DLL getShapeTypeFromString(std::string str);
-	namespace Tools
-	{
-		namespace Geometry //for testing in gemometry
-		{
-			///Return the distance between the 2 objects centers
-			float DLL distance(Annwvyn::AnnGameObject* a, Annwvyn::AnnGameObject* b);
-
-			///Return the distance between 2 points in space
-			float DLL distance(Ogre::Vector3 a, Ogre::Vector3 b);
-		}
-	}
+	
 }
 
 #endif
--- src/AnnTools.cpp
+++ src/AnnTools.cpp
@@ -1,30 +1,6 @@
 #include "stdafx.h"
 #include "AnnTools.h"
 
-using namespace Ogre;
-
-float Annwvyn::Tools::Geometry::distance(Annwvyn::AnnGameObject* a, Annwvyn::AnnGameObject* b)
-{
-	return Annwvyn::Tools::Geometry::distance(a->getPosition(), b->getPosition());
-}
-
-float Annwvyn::Tools::Geometry::distance(Ogre::Vector3 a, Ogre::Vector3 b)
-{
-	//get a vector representing translation between the two objects
-	Vector3 vector = b-a; 
-	
-	//store math result :
-	float dist;
-	
-	//do dist = x² + y² + z²
-	dist = vector.x * vector.x;
-	dist += vector.y * vector.y;
-	dist += vector.z * vector.z;
-	
-    return sqrt(dist); 
-}
-
-
 Annwvyn::phyShapeType Annwvyn::getShapeTypeFromString(std::string str)
 {
 	if(str == "static")
