0b149d87f5fd6c5b14c723f622980d7329409ea5
Arthur Brainville <ybalrid@ybalrid.info>
2016-08-27 18:26:45 +0200

    Update doxygen_dummy.hpp

--- include/doxygen_dummy.hpp
+++ include/doxygen_dummy.hpp
@@ -2,6 +2,11 @@
 
 namespace std
 {
+  ///standard shared pointer from the <memory> header. Dummy redefiniton.
   template<class T> class shared_ptr{ T* dummy };
+  ///standard weak pointer from the <memory> header. Dummy redefiniton.
   template<class T> class weak_ptr{ T* dummy };
+  ///standard unique pointer from the <memory> header. Dummy redefiniton.
+  template<class T> class unique_ptr{ T* dummy };
+  
 }
