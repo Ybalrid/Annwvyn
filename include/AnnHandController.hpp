ac3860508b1747e795a13f0fbe1b68e2e15c5476
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2016-09-11 04:51:13 +0200

    HandController impl cleanly put in .cpp file

--- include/AnnHandController.hpp
+++ include/AnnHandController.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Ogre.h>
+#include "systemMacro.h"
 #include "AnnVect3.hpp"
 #include "AnnQuaternion.hpp"
 
@@ -9,114 +10,80 @@ namespace Annwvyn
 {
 
 	typedef size_t AnnHandControllerID;
-	class AnnHandController
+	class DLL AnnHandController
 	{
 	public:
+		///Identify the controller as "left hand", "right hand" or "invalid hand"
 		enum AnnHandControllerSide{leftHandController, rightHandController, invalidHandController};
-		AnnHandController(Ogre::SceneNode* handNode, AnnHandControllerID controllerID, AnnHandControllerSide controllerSide) :
-			node(handNode),
-			id(controllerID),
-			side(controllerSide),
-			model(nullptr),
-			grabbed(nullptr),
-			tracked(false),
-			trackedAngularSpeed(AnnVect3::ZERO),
-			trackedLinearSpeed(AnnVect3::ZERO)
-		{
-			std::cerr << "HandController ID : " << id << " created"; 
-			std::cerr << "For side : " << getSideAsString(side);
-		}
 
-		inline std::string getSideAsString(AnnHandControllerSide s)
-		{
-			if (s == leftHandController) return "Left Hand";
-			return "Right Hand";
-		}
+		///Construct a Controller object
+		AnnHandController(Ogre::SceneNode* handNode, AnnHandControllerID controllerID, AnnHandControllerSide controllerSide);
 
-		void attachModel(Ogre::Entity* handModel)
-		{
-			if(model) node->detachObject(model);
-			model = handModel;
-			node->attachObject(model);
-		}
+		///Get the side type as a std::strinng
+		inline std::string getSideAsString(AnnHandControllerSide s);
 
-		void detachModel()
-		{
-			if (model) node->detachObject(model);
-			model = nullptr;
-		}
+		///Attach a 3D model to the hand. Previously attached model will be detached
+		void attachModel(Ogre::Entity* handModel);
 
-		AnnVect3 getWorldPosition()
-		{
-			return node->getPosition();
-		}
+		///Detach model without destroying it
+		void detachModel();
 
-		AnnQuaternion getWorldOrientation()
-		{
-			return node->getOrientation();
-		}
+		///Return the current model :
+		Ogre::Entity* getModel();
 
-		AnnVect3 getAngularSpeed()
-		{
-			return trackedAngularSpeed;
-		}
+		///Get position in world space
+		AnnVect3 getWorldPosition();
 
-		AnnVect3 getLinearSpeed()
-		{
-			return trackedLinearSpeed;
-		}
+		///Get orientaiton in world space
+		AnnQuaternion getWorldOrientation();
 
-		AnnVect3 getPointingDirection()
-		{
-			return node->getOrientation() * AnnVect3::NEGATIVE_UNIT_Z;
-		}
+		///Get tracked angular speed
+		AnnVect3 getAngularSpeed();
 
-		void attachNode(Ogre::SceneNode* grabbedObject)
-		{
-			if (grabbedObject->getParentSceneNode())
-				grabbedObject->getParentSceneNode()->removeChild(grabbedObject);
-			node->addChild(grabbedObject);
-			grabbed = grabbedObject;
-		}
+		///Get tracked linear speed
+		AnnVect3 getLinearSpeed();
 
-		void setTrackedPosition(AnnVect3 position)
-		{
-			tracked = true;
-			node->setPosition(position);
-		}
+		///Get a vector aligned with the pointing direction
+		AnnVect3 getPointingDirection();
 
-		void setTrackedOrientation(AnnQuaternion orientation)
-		{
-			tracked = true;
-			node->setOrientation(orientation);
-		}
+		///Attach the node as a child to the controller node
+		void attachNode(Ogre::SceneNode* grabbedObject);
 
-		void setTrackedLinearSpeed(AnnVect3 v)
-		{
-			tracked = true;
-			trackedLinearSpeed = v;
-		}
+		///Set the position of the hand 
+		void setTrackedPosition(AnnVect3 position);
 
-		void setTrackedAngularSpeed(AnnVect3 v)
-		{
-			tracked = true;
-			trackedAngularSpeed = v;
-		}
+		///Set the orientaiton of the hand
+		void setTrackedOrientation(AnnQuaternion orientation);
 
-		bool isTracked()
-		{
-			return tracked;
-		}
+		///Set the linear velocity of the hand
+		void setTrackedLinearSpeed(AnnVect3 v);
+
+		///Set the angular velocity of the hand
+		void setTrackedAngularSpeed(AnnVect3 v);
+
+		///Return true if the hand controller object has recived updates from the tracking system
+		bool isTracked();
 
 	private:
+		///ID of the controller, expect 1 or 2 
 		AnnHandControllerID id;
+
+		///Side of the controller, right hand? left hand? Who knows!
 		AnnHandControllerSide side;
 
+		///Some Ogre Scene Nodes
 		Ogre::SceneNode* node, * grabbed;
+
+		///Currently attached entity
 		Ogre::Entity* model;
+
+		///tracked boolean, true if controller has been updated by the engine
 		bool tracked;
 
+		///Angular velocity (euler?) vector
 		AnnVect3 trackedAngularSpeed; 
+
+		///Linear velocity vector
 		AnnVect3 trackedLinearSpeed;
 	};
 }
