d57f5c2f04177224d1f879bfe5977eafa0093462
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2016-09-13 11:28:51 +0200

    Add the possibility to register an user defined subsystem

---
+++ src/AnnUserSpaceSubSystem.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "AnnUserSpaceSubSystem.hpp"
+#include "AnnLogger.hpp"
+
+using namespace Annwvyn;
+
+AnnUserSubSystem::AnnUserSubSystem(std::string systemName) : AnnSubSystem(systemName)
+{
+	AnnDebug() << "^^^^^ This system is user defined.";
+}
+
+void Annwvyn::AnnUserSubSystem::update()
+{
+}
+
+bool Annwvyn::AnnUserSubSystem::needUpdate()
+{
+	return false;
+}
