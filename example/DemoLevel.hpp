d4717b7f580b34be44d1823533ab049b0d2cee2b
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2016-09-26 03:56:34 +0200

    Start the basis of a hub to go try demos of the engine

---
+++ example/DemoLevel.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Annwvyn.h>
+using namespace Annwvyn;
+
+class DemoHub : LEVEL
+{
+public:
+	void load()
+	{
+
+	}
+
+	void runLogic()
+	{
+
+	}
+};
+
+class Demo0 : LEVEL
+{
+public:
+	void load()
+	{
+
+	}
+
+	void runLogic()
+	{
+
+	}
+};
