5c6f0a26b7c4fb07130b5467ead436f4bb44dbbe
Ybalrid <ybalrid@cecmu.org>
2016-10-01 20:38:55 +0200

    add documentation comment for movable

--- include/AnnAbstractMovable.hpp
+++ include/AnnAbstractMovable.hpp
@@ -5,6 +5,7 @@
 
 namespace Annwvyn
 {
+	///Abstract Class that represent a movable object. Define the API used to move simple objects in 3D space 
 	class DLL AnnAbstractMovable
 	{
 		virtual void setPosition(AnnVect3 position) = 0;
