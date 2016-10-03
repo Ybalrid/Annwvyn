e72a89ee0c571e3a0769a16ac2938c3daa3c4801
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2016-10-03 02:22:45 +0200

    mdocument user subsystem class

--- include/AnnUserSpaceSubSystem.hpp
+++ include/AnnUserSpaceSubSystem.hpp
@@ -10,13 +10,20 @@
 namespace Annwvyn
 {
 	class AnnEngine;
+	///User defined subsystem base class 
 	class DLL AnnUserSubSystem : public AnnSubSystem
 	{
 	public:
+		///Construct the user subsystem. The systemName is mandatory
+		/// \param systemName The name of the subsystem. Should be unique.
 		AnnUserSubSystem(std::string systemName);
 	protected:
 		friend class AnnEngine;
+
+		///Called at each refresh if this->needUpdate() returns true
 		virtual void update();
+
+		///Marker if the system wants it's "update" method to be called by the engine. this is tested each frames. 
 		virtual bool needUpdate();
 	};
 }
