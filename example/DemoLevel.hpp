b45761b316f37edea9940abf65af8a0a68173d02
Ybalrid <ybalrid@cecmu.org>
2016-10-09 19:06:41 +0200

    add the old test level to the demo hub system

--- example/DemoLevel.hpp
+++ example/DemoLevel.hpp
@@ -30,27 +30,48 @@ public:
 		auto Ground = addGameObject("Ground.mesh");
 		Ground->setUpPhysics();
 
-		auto Stone = addGameObject("DemoStone.mesh");
-		Stone->setPosition({ -2, 0, -4 });
-		Stone->setOrientation(AnnQuaternion(Ogre::Degree(45), AnnVect3::UNIT_Y));
-		Stone->setUpPhysics();
+		auto StoneDemo0 = addGameObject("DemoStone.mesh");
+		StoneDemo0->setPosition({ -3, 0, -5 });
+		StoneDemo0->setOrientation(AnnQuaternion(AnnDegree(45), AnnVect3::UNIT_Y));
+		StoneDemo0->setUpPhysics();
 
 		auto TextPane = make_shared<Ann3DTextPlane>(2.f, 1.f, "Demo 0\nDemo the loading of a demo... xD", 512, 18.f);
 		TextPane->setTextAlign(Ann3DTextPlane::ALIGN_CENTER);
 		TextPane->setTextColor(AnnColor{ 0, 0, 0 });
-		TextPane->setPosition(Stone->getPosition() + Stone->getOrientation()*  AnnVect3{ 0, 2, -0.35 });
-		TextPane->setOrientation(Stone->getOrientation());
+		TextPane->setPosition(StoneDemo0->getPosition() + StoneDemo0->getOrientation()*  AnnVect3{ 0, 2, -0.35 });
+		TextPane->setOrientation(StoneDemo0->getOrientation());
 		TextPane->update();
 		addManualMovableObject(TextPane);
-		
-		auto Sun = addLightObject();
-		Sun->setType(AnnLightObject::ANN_LIGHT_DIRECTIONAL);
-		Sun->setDirection({ -0.5, -1, -0.5 });
 
 		auto Demo0Trigger = static_pointer_cast<AnnSphericalTriggerObject>(addTrggerObject());
 		Demo0Trigger->setThreshold(1.5f);
-		Demo0Trigger->setPosition(Stone->getPosition() + AnnVect3(0, 0.5f, 0));
+		Demo0Trigger->setPosition(StoneDemo0->getPosition() + AnnVect3(0, 0.5f, 0));
 		demo0trig = Demo0Trigger;
+
+		auto StoneTestLevel = addGameObject("DemoStone.mesh");
+		StoneTestLevel->setPosition({ +3, 0, -5 });
+		StoneTestLevel->setOrientation(AnnQuaternion(AnnDegree(-45), AnnVect3::UNIT_Y));
+		StoneTestLevel->setUpPhysics();
+
+		auto TestLevelText = make_shared<Ann3DTextPlane>(2.f, 1.f, "TestLevel\nA simple test level", 512, 18.f);
+		TestLevelText->setTextAlign(Ann3DTextPlane::ALIGN_CENTER);
+		TestLevelText->setTextColor(AnnColor{ 0, 0, 0 });
+		TestLevelText->setPosition(StoneTestLevel->getPosition() + StoneTestLevel->getOrientation()*  AnnVect3{ 0, 2, -0.35 });
+		TestLevelText->setOrientation(StoneTestLevel->getOrientation());
+		TestLevelText->update();
+		addManualMovableObject(TestLevelText);
+
+		auto TestLevelTrigger = static_pointer_cast<AnnSphericalTriggerObject>(addTrggerObject());
+		TestLevelTrigger->setThreshold(1.5f);
+		TestLevelTrigger->setPosition(StoneTestLevel->getPosition() + AnnVect3(0, 0.5f, 0));
+		testLevelTrig = TestLevelTrigger;
+		
+		auto Sun = addLightObject();
+		Sun->setType(AnnLightObject::ANN_LIGHT_DIRECTIONAL);
+		Sun->setDirection({ 0, -1, -0.5 });
+
+		AnnGetSceneryManager()->setAmbiantLight(AnnColor(0.25f, 0.25f, 0.25f));
+
 
 		AnnGetPlayer()->setPosition({ 0, 1, 0 });
 		AnnGetPlayer()->setOrientation(Ogre::Euler(0));
@@ -78,7 +99,14 @@ public:
 	void jumpToLevelTriggeredBy(std::shared_ptr<AnnTriggerObject> trigger)
 	{
 		if (demo0trig == trigger)
+		{
 			AnnGetLevelManager()->jump(getDemo(0));
+			return;
+		}
+		if (testLevelTrig == trigger)
+		{
+			AnnGetLevelManager()->jump(getDemo(1));
+		}
 	}
 
 	level_id getDemo(level_id id)
@@ -88,6 +116,7 @@ public:
 
 private:
 	std::shared_ptr<AnnTriggerObject> demo0trig;
+	std::shared_ptr<AnnTriggerObject> testLevelTrig;
 
 };
 
