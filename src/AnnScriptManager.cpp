263b3f9896ed88f8d504b71fa8cf671b43ed9e04
Ybalrid <ybalrid@cecmu.org>
2016-10-29 17:27:05 +0200

    Add more vector, angles and quaternions to the script API

--- src/AnnScriptManager.cpp
+++ src/AnnScriptManager.cpp
@@ -82,35 +82,88 @@ void Annwvyn::AnnScriptManager::registerApi()
 	using namespace chaiscript;
 	//TODO Add to chai all the useful types (angles, vectors, quaternions...)
 
-	//Base Class of AnnVect3
-	chai.add(chaiscript::user_type<Ogre::Vector3>(), "AnnVect3");
-	chai.add(chaiscript::constructor<Ogre::Vector3()>(), "AnnVect3");
-	chai.add(chaiscript::constructor<Ogre::Vector3(const float, const float, const float)>(), "AnnVect3");
-	chai.add(chaiscript::constructor<Ogre::Vector3(const float[3])>(), "AnnVect3");
-	chai.add(chaiscript::constructor<Ogre::Vector3(const Ogre::Vector3&)>(), "AnnVect");
-	//Public attributes. Thank the simulation authors, this is easy
-	chai.add(chaiscript::fun(&Ogre::Vector3::x), "x");
-	chai.add(chaiscript::fun(&Ogre::Vector3::y), "y");
-	chai.add(chaiscript::fun(&Ogre::Vector3::z), "z");
+	// 3D vector
+	chai.add(user_type<Vector3>(), "AnnVect3");
+	chai.add(constructor<Vector3()>(), "AnnVect3");
+	chai.add(constructor<Vector3(const float, const float, const float)>(), "AnnVect3");
+	chai.add(constructor<Vector3(const float[3])>(), "AnnVect3");
+	chai.add(constructor<Vector3(const Vector3&)>(), "AnnVect");
+	// (x,y,z)
+	chai.add(fun(&Vector3::x), "x");
+	chai.add(fun(&Vector3::y), "y");
+	chai.add(fun(&Vector3::z), "z");
+	// arithmetic operators
+	chai.add(fun([](Vector3& u, const Vector3& v) {u = v; }), "=");
+	chai.add(fun<Vector3>(&Vector3::operator+), "+");
+	chai.add(fun([](const Vector3& v) {return -v; }), "-");
+	chai.add(fun([](const Vector3& v, Vector3 w) {return v - w; }), "-");
+	chai.add(fun([](const Vector3& vector, Real scalar) {return scalar*vector; }), "*");
+	chai.add(fun([](Vector3& vector, Real scalar) {vector *= scalar; }), "*=");
+	// conditional operators
 
-	//the lambda here is a hack because I can't find a way to give chai operator= cleanly
-	chai.add(chaiscript::fun([](Ogre::Vector3& u, const Ogre::Vector3& v) {u = v; }), "=");
+	//Angles
+	chai.add(user_type<Radian>(), "AnnRadian");
+	chai.add(user_type<Degree>(), "AnnDegree");
+	chai.add(constructor<Radian(Real)>(), "AnnRadian");
+	chai.add(constructor<Degree(Real)>(), "AnnDegree");
+	chai.add(constructor<Radian(const Degree&)>(), "AnnRadian");
+	chai.add(constructor<Degree(const Radian&)>(), "AnnDegree");
 
-	//This works here, that's weird than operator- and operator= don't want to
-	chai.add(chaiscript::fun<Ogre::Vector3>(&Ogre::Vector3::operator+), "+");
-	//There's multiple overloads of Ogre::Vector3::operator-. Easier to use a lambda here too
-	chai.add(chaiscript::fun([](const Ogre::Vector3& v) {return -v; }), "-");
-	chai.add(chaiscript::fun([](const Ogre::Vector3& v, Ogre::Vector3 w) {return v - w; }), "-");
+	chai.add(fun([](const Degree& d) {return +d; }), "+");
+	chai.add(fun([](const Degree& d1, const Degree& d2) {return d1 + d2; }), "+");
+	chai.add(fun([](const Degree& d1, const Radian& d2) {return d1 + d2; }), "+");
+	chai.add(fun([](Degree& d1, const Degree& d2) {d1 += d2; }), "+=");
+	chai.add(fun([](const Degree& d) {return -d; }), "-");
+	chai.add(fun([](const Degree& d1, const Degree& d2) { return d1 - d2; }), "-");
+	chai.add(fun([](const Degree& d1, const Radian& d2) { return d1 - d2; }), "-");
+	chai.add(fun([](Degree& d1, const Degree& d2) {d1 -= d2; }), "-=");
+	chai.add(fun([](Degree& d1, const Radian& d2) {d1 -= d2; }), "-=");
+	chai.add(fun([](const Degree& d, Real f) {return d*f; }), "*");
+	chai.add(fun([](Degree& d, Real f) {d *= f; }), "*=");
+	chai.add(fun([](const Degree& d, Real f) {return d / f; }), "/");
+	chai.add(fun([](Degree& d, Real f) {d /= f; }), "/=");
 
-	//note to self, at this point, using lambdas seems easier than trying to bind the operator overloads to chaiscript operators
-	chai.add(chaiscript::fun([](const Ogre::Vector3& vector, Ogre::Real scalar) {return scalar*vector; }), "*");
+	chai.add(fun([](const Degree& d1, const Degree& d2) {d1 < d2; }), "<");
+	chai.add(fun([](const Degree& d1, const Degree& d2) {d1 > d2; }), ">");
+	chai.add(fun([](const Degree& d1, const Degree& d2) {d1 <= d2; }), "<=");
+	chai.add(fun([](const Degree& d1, const Degree& d2) {d1 >= d2; }), ">=");
+	chai.add(fun([](const Degree& d1, const Degree& d2) {d1 == d2; }), "==");
+	chai.add(fun([](const Degree& d1, const Degree& d2) {d1 != d2; }), "!=");
 
-	//OMG, it just works now. I <3 Lambdas
-	chai.add(chaiscript::fun([](Ogre::Vector3& vector, Ogre::Real scalar) {vector *= scalar; }), "*=");
+	chai.add(fun([](const Radian& d) {return +d; }), "+");
+	chai.add(fun([](const Radian& d1, const Radian& d2) {return d1 + d2; }), "+");
+	chai.add(fun([](const Radian& d1, const Degree& d2) {return d1 + d2; }), "+");
+	chai.add(fun([](Radian& d1, const Radian& d2) {d1 += d2; }), "+=");
+	chai.add(fun([](const Radian& d) {return -d; }), "-");
+	chai.add(fun([](const Radian& d1, const Radian& d2) { return d1 - d2; }), "-");
+	chai.add(fun([](const Radian& d1, const Degree& d2) { return d1 - d2; }), "-");
+	chai.add(fun([](Radian& d1, const Radian& d2) {d1 -= d2; }), "-=");
+	chai.add(fun([](Radian& d1, const Degree& d2) {d1 -= d2; }), "-=");
+	chai.add(fun([](const Radian& d, Real f) {return d*f; }), "*");
+	chai.add(fun([](Radian& d, Real f) {d *= f; }), "*=");
+	chai.add(fun([](const Radian& d, Real f) {return d / f; }), "/");
+	chai.add(fun([](Radian& d, Real f) {d /= f; }), "/=");
 
+	chai.add(fun([](const Radian& d1, const Radian& d2) {return d1 < d2; }), "<");
+	chai.add(fun([](const Radian& d1, const Radian& d2) {return d1 > d2; }), ">");
+	chai.add(fun([](const Radian& d1, const Radian& d2) {return d1 <= d2; }), "<=");
+	chai.add(fun([](const Radian& d1, const Radian& d2) {return d1 >= d2; }), ">=");
+	chai.add(fun([](const Radian& d1, const Radian& d2) {return d1 == d2; }), "==");
+	chai.add(fun([](const Radian& d1, const Radian& d2) {return d1 != d2; }), "!=");
+
+	//Quaternions
 	chai.add(user_type<Quaternion>(), "AnnQuat");
 	chai.add(constructor<Quaternion()>(), "AnnQuat");
 	chai.add(constructor<Quaternion(const float, const float, const float, const float)>(), "AnnQuat");
+	chai.add(constructor<Quaternion(Radian, Vector3)>(), "AnnQuat");
+	chai.add(constructor<Quaternion(Vector3, Vector3, Vector3)>(), "AnnQuat");
+	chai.add(fun(&Quaternion::x), "x");
+	chai.add(fun(&Quaternion::y), "y");
+	chai.add(fun(&Quaternion::z), "z");
+	chai.add(fun(&Quaternion::w), "w");
+
+	chai.add(fun([](const Quaternion& q, const Vector3 v) {return q * v; }), "*");
+	chai.add(fun([](const Quaternion& q1, const Quaternion& q2) {return q1 * q2; }), "*");
 
 	//TODO Add to chai a way to access useful Annwvyn components
 }
@@ -128,11 +181,6 @@ Annwvyn::AnnBehaviourScript::AnnBehaviourScript(std::string scriptName, std::fun
 	ScriptObjectInstance(chaisriptInstance)
 {
 }
-
-//Annwvyn::AnnBehaviourScript::AnnBehaviourScript(bool invalid) : valid(invalid),
-//ScriptObjectInstance(chaiscript::Boxed_Value(false))
-//{
-//}
 
 void Annwvyn::AnnBehaviourScript::update()
 {
