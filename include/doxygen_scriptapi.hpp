049a951a1dc39cdb9d55718240c7bd268841ab3a
Ybalrid <ybalrid@cecmu.org>
2016-11-06 01:48:22 +0100

    insert some classes into the documentation of the scripting API

--- include/doxygen_scriptapi.hpp
+++ include/doxygen_scriptapi.hpp
@@ -10,6 +10,7 @@
 namespace Annwvyn
 {
 	//As a safety measure, if anybody include this file even with the warning in the 1st line, it will put it's junk in a sub-namespace
+	//As a side effect of the above statement, this permit to access the whole thing via a single link on the generated website. GREAT.
 	///List prototypes of C++ non-existing functions that match the interface of the Script functions you can use!
 	namespace ChaiScriptAPIDoc
 	{
@@ -41,6 +42,28 @@ namespace Annwvyn
 		/// \param c Color to log
 		void AnnDebugLog(const AnnColor& c);
 
+		///ScriptFunction: Log a keycode
+		/// \param c KeyCode number
+		void AnnDebugLog(KeyCode::code c);
+
+		///ScriptFunction: Log a mouse axis
+		/// \param a Axis
+		void AnnDebugLog(MouseAxisId a);
+
+		///ScriptFunction: log a boolean
+		/// \param b bool to log
+		void AnnDebugLog(bool b);
+
+		///ScriptFunction: log an integer
+		/// \param i int
+		void AnnDebugLog(int i);
+
+		///ScriptFunction: log a simple precision floating point number
+		/// \param f float
+		void AnnDebugLog(float f);
+
+		//-----------------------------------------------------------------------
+
 		///ScritpFunciton: Set the world's background color
 		/// \param c Color
 		void AnnSetWorldBackgroundColor(const AnnColor& c);
@@ -54,7 +77,7 @@ namespace Annwvyn
 		/// \param objectName Name that the object will bear
 		void AnnCreateGameObject(const std::string& mesh, const std::string& objectName);
 
-		///ScriptFunction: Remove a game boject. Will be removed from the current level if a level is running
+		///ScriptFunction: Remove a game object. Will be removed from the current level if a level is running
 		/// \param objectName Name of the object
 		void AnnRemoveGameObject(const std::string& objectName);
 
@@ -72,5 +95,175 @@ namespace Annwvyn
 		///ScriptFunction: Jump the level manager to another level
 		/// \param id The ID number of the level
 		void AnnJumpLevel(Annwvyn::level_id id);
+
+		//-classes-----------------------------------------------------
+
+		///Keyboard event from scripts
+		class AnnKeyEvent
+		{
+		public:
+			///Return if event is press event
+			bool isPressed();
+
+			///Return if event is released event
+			bool isReleased();
+
+			///Key concerned by this event
+			KeyCode::code getKey();
+		};
+
+		///Mouse event from script
+		class AnnMouseEvent
+		{
+		public:
+			///Axis of the mouse 0 is horizontal, 1 is vertical and 2 is scrolling
+			AnnStickAxis getAxis(const int id);
+
+			///State of a button, left, right, middle, and others, maybe...
+			bool getButtonState(const int id);
+		};
+
+		///Axis of a mouse
+		class AnnMouseAxis
+		{
+		public:
+			///get relative value
+			int getRelValue();
+
+			///get absolute value
+			int getAbsValue();
+		};
+
+		///Joystick event
+		class AnnStickEvent
+		{
+		public:
+			///return how many buttons
+			size_t getNbButtons();
+
+			///return how many axes
+			size_t getNbAxis();
+
+			///return how many PoV
+			size_t getNbPov();
+
+			///return the vendor string of this controller. In practice it's the name of the controller
+			std::string getVendor();
+
+			///Get the ID number of this controller
+			unsigned int getSitckID();
+
+			///Return true if this controller is an xbox controller. Usefull for oculus rift games...
+			bool isXboxController();
+
+			///Is button `i` pressed
+			bool isPressed(const int i);
+
+			///Is button `i` released
+			bool isReleased(const int i);
+
+			///Is button `i` <strong>currently down</strong>
+			bool isDown(const int i);
+
+			///Get the wanted axis
+			AnnStickAxis getAxis(const int i);
+
+			///Get the wanted pov
+			AnnStickPov getPov(const int i);
+		};
+
+		///state of a PoV from a controller
+		class AnnStickPov
+		{
+		public:
+			///N
+			bool getNorth();
+			///S
+			bool getSouth();
+			///E
+			bool getEast();
+			///W
+			bool getWest();
+			/// N && E
+			bool getNorthEast();
+			/// N && W
+			bool getNorthWest();
+			/// S && E
+			bool getSouthEast();
+			/// S && W
+			bool getSouthWest();
+		};
+
+		///Wait for a timer to go to 0
+		class AnnTimeEvent
+		{
+		public:
+			///Get the ID of the timer that timeouted
+			timerID getID();
+		};
+
+		///A color
+		class AnnColor
+		{
+			///Construct with RGBA
+			AnnColor(float r, float g, float b, float a);
+			///Construct with a color from Ogre
+			AnnColor(const Ogre::ColourValue& c);
+			///Asign another color to this one
+			AnnColor operator=(AnnColor& c);
+
+			///Get Red as [0;1] float
+			float getRed();
+
+			///Get Green as [0;1] float
+			float getGreen();
+
+			///Get Blue as [0;1] float
+			float getBlue();
+
+			///Get Alpha as [0;1] float
+			float getAlpha();
+
+			///Set Red as [0;1] float
+			void setRed(float f);
+			///Set Green as [0;1] float
+			void setGreen(float f);
+			///Set Blue as [0;1] float
+			void setBlue(float f);
+			///Set Alpha as [0;1] float
+			void setAlpha(float f);
+		};
+
+		///A game object
+		class AnnGameObject
+		{
+		public:
+			///Set the position of this object
+			void setPosition(AnnVect3 v);
+
+			///Set the orientation of this object
+			void setOrientation(AnnQuaternion q);
+
+			///Set the scaling of this object
+			void setScale(AnnVect3 v);
+
+			///Get the position of this object
+			AnnVect3 getPosition();
+
+			///Get the orientation of this object
+			AnnQuaternion getOrientation();
+
+			///Get the scale of this object
+			AnnVect3 getScale();
+
+			///Play a sound
+			void playSound(std::string name, bool loop = true);
+		};
+
+		///Value of Pi
+		float PI;
+
+		///Value of Pi/2
+		float HALF_PI;
 	}
 }
