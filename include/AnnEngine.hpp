b542039db29521ed0a20f5f64da89a18350ef566
Ybalrid <ybalrid@cecmu.org>
2017-01-26 15:42:42 +0100

    prepare for 0.3.0

--- include/AnnEngine.hpp
+++ include/AnnEngine.hpp
@@ -10,8 +10,8 @@
 
 //Keep track of engine version here
 #define ANN_MAJOR 0
-#define ANN_MINOR 2
-#define ANN_PATCH 6
+#define ANN_MINOR 3
+#define ANN_PATCH 0
 #define ANN_EXPERIMENTAL true
 
 #include "systemMacro.h"
