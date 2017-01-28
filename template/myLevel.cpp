65c6e43a7643f1fc3aa616fb20eccbd6f13f6be4
Ybalrid <ybalrid@cecmu.org>
2017-01-28 03:35:51 +0100

    Improve template project

--- template/myLevel.cpp
+++ template/myLevel.cpp
@@ -1,16 +1,20 @@
 #include "stdafx.h"
 #include "myLevel.hpp"
 
+using namespace Annwvyn;
+
 MyLevel::MyLevel() : constructLevel()
 {
 }
 
 void MyLevel::load()
 {
-	//Load Sinbad:
+	//Create Sinbad the Ogre
 	auto Sinbad(addGameObject("Sinbad.mesh"));
 	Sinbad->setScale({ 0.3f, 0.3f, 0.3f });
-	Sinbad->setPosition({ 0,1,0 });
+	Sinbad->setPosition({ 0, 1, 0 });
+	//Setting a non-null mass and a non static geometric primitive make this object
+	//a dynamic physics object (it can move if forces are applied upon him)
 	Sinbad->setUpPhysics(250, boxShape);
 
 	//Load Ground:
@@ -21,10 +25,12 @@ void MyLevel::load()
 	//Create a light source
 	auto Sun(addLightObject());
 	Sun->setType(AnnLightObject::ANN_LIGHT_DIRECTIONAL);
+
 	//zenith sunlight
 	Sun->setDirection(AnnVect3{ -1, -1.5f, -1 }.normalisedCopy());
 
-	AnnGetSceneryManager()->setAmbientLight(AnnColor(.25f, .25f, .25));
+	//The ambient light is a scene light that is emitted by every point of the scene. This is for making the shadows not pitch black.
+	AnnGetSceneryManager()->setAmbientLight(AnnColor(.25f, .25f, .25f));
 }
 
 void MyLevel::runLogic()
