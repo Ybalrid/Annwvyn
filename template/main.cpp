3f601eece51ef6064393da312bf4a0781b8e97dc
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-01-28 03:35:51 +0100

    Improve template project

--- template/main.cpp
+++ template/main.cpp
@@ -3,20 +3,29 @@
 
 //Include Annwvyn Engine.
 #include <Annwvyn.h>
+
 //Every Annwvyn classes are in the Annwvyn namespace
 using namespace Annwvyn;
 
 //Include our level/stages here
 #include "myLevel.hpp"
 
+//This main function job here is to init the engine,
+//create and load one 'Level' object,
+//and set to use the built in "event listener" for controls
+
+//All the resources here have been put in the "media" directory
+//that is auto loaded by Annwvyn
+
 AnnMain() //The application entry point is "AnnMain()". return type int.
 {
 	//Initialize the engine
-	AnnEngine::openConsole();//optional : open console
-	AnnInit("NameOfApp");
+	//AnnEngine::openConsole(); //optional : open console in terminal window;
+
+	AnnInit("MyVRGame");
 
 	//Call physics initialization for the player body:
-	AnnGetEngine()->initPlayerPhysics();
+	AnnGetEngine()->initPlayerStandingPhysics(); //There's an optional "room scale" physics mode too, but it's pretty beta right now.
 
 	//Instantiate and register our basic level and "jump" to it:
 	AnnGetLevelManager()->addLevel(std::make_shared<MyLevel>());
