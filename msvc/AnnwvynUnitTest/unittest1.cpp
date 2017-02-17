b45605176777698832d984f4b009de9370ad7f58
Ybalrid <ybalrid@cecmu.org>
2017-02-17 16:47:22 +0100

    push unit test project

---
+++ msvc/AnnwvynUnitTest/unittest1.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace AnnwvynUnitTest
+{
+	TEST_CLASS(UnitTestExample)
+	{
+	public:
+
+		/*
+		 TEST_METHOD(ExamplePassTest)
+		{
+			// TODO: Your test code here
+			Assert::AreEqual(1, 1, L"Test that should pass : 1 == 1 ", LINE_INFO());
+		}
+
+		TEST_METHOD(ExampleFailTest)
+		{
+			Assert::AreEqual(2 + 2, 5, L"Test that should fail : 2 + 2 == 5", LINE_INFO());
+		}
+		*/
+	};
+}
