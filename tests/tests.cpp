7a66ff9d804f587dd6a53ff3f025ce374b776efe
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-07-31 03:47:27 +0200

    some console fix on AnnEngine's behavior

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,7 @@
 
 //Include the engine itself
 #include <Annwvyn.h>
+#define RENDERER "OgreNoVRRender"
 
 #define CATCH_CONFIG_RUNNER
 #include <catch/catch.hpp>
@@ -11,9 +12,11 @@ void consolePause();
 
 int main(int argc, char* argv[])
 {
+	Annwvyn::AnnEngine::setNoConsoleColor();
+
 	auto result = Catch::Session().run(argc, argv);
 
-	consolePause();
+	//consolePause();
 
 	return (result < 0xff ? result : 0xff);
 }
@@ -22,4 +25,24 @@ void consolePause()
 {
 	std::cout << "Press RETURN to continue... ";
 	std::cin.get();
+}
+
+TEST_CASE("Test 1")
+{
+	REQUIRE(1 == 1);
+}
+
+TEST_CASE("BASIC ENGINE START")
+{
+	auto GameEngine = std::make_unique<Annwvyn::AnnEngine>("UnitTest0", RENDERER);
+	REQUIRE(GameEngine != nullptr);
+}
+
+TEST_CASE("BASIC ENGINE START WITH RENDER")
+{
+	auto GameEngine = std::make_unique<Annwvyn::AnnEngine>("UnitTest1", RENDERER);
+	REQUIRE(GameEngine != nullptr);
+
+	for (auto i = 0; i < 60 * 5; i++)
+		GameEngine->refresh();
 }
