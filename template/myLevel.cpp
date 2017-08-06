1d5052839d7a35c70b6ad7ab7f1266e1a4327a5e
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-08-06 23:46:57 +0200

    just checked how the template project was behaving

--- template/myLevel.cpp
+++ template/myLevel.cpp
@@ -11,8 +11,9 @@ void MyLevel::load()
 {
 	//Create Sinbad the Ogre
 	auto Sinbad(addGameObject("Sinbad.mesh"));
-	Sinbad->setScale({ 0.3f, 0.3f, 0.3f });
+	Sinbad->setScale(0.5f * AnnVect3::UNIT_SCALE);
 	Sinbad->setPosition({ 0, 1, 0 });
+
 	//Setting a non-null mass and a non static geometric primitive make this object
 	//a dynamic physics object (it can move if forces are applied upon him)
 	Sinbad->setUpPhysics(250, boxShape);
@@ -21,11 +22,12 @@ void MyLevel::load()
 	auto Ground(addGameObject("floorplane.mesh"));
 	Ground->setPosition({ 0, 0, 0 });
 	Ground->setUpPhysics();
-    AnnGetPlayer()->regroundOnPhysicsBody();
+	AnnGetPlayer()->regroundOnPhysicsBody();
 
 	//Create a light source
 	auto Sun(addLightObject("MySun"));
 	Sun->setType(AnnLightObject::ANN_LIGHT_DIRECTIONAL);
+	Sun->setDirection(AnnVect3{ -0.125, -1, -0.5 }.normalisedCopy());
 	Sun->setPower(97);
 
 	auto OtherLight(addLightObject("Other Light"));
