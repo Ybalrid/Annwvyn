3ecb6483ebb31269e88991344b1f2ca69a77922b
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-09-18 04:08:13 +0200

    remove useless scoping

--- tests/AnnScriptInterfaceTest.cpp
+++ tests/AnnScriptInterfaceTest.cpp
@@ -5,34 +5,32 @@ namespace Annwvyn
 {
 	TEST_CASE("Attach script to object")
 	{
+		auto GameEngine = bootstrapEmptyEngine("TestScript");
+
+		auto ResourceManager = AnnGetResourceManager();
+		ResourceManager->addFileLocation("./unitTestScripts");
+		ResourceManager->initResources();
+
+		auto GameObjectManager = AnnGetGameObjectManager();
+		auto sun = GameObjectManager->createLightObject();
+		sun->setType(AnnLightObject::ANN_LIGHT_DIRECTIONAL);
+		sun->setDirection({ 0, -5, -1 });
+		sun->setPower(97);
+		auto floor = GameObjectManager->createGameObject("floorplane.mesh", "Floor");
+		auto ogre = GameObjectManager->createGameObject("Sinbad.mesh", "Ogre");
+
+		//Attach the behavior that cause to move the object by +0.02f on +y every frame
+		ogre->attachScript("GoUpBehavior");
+
+		//run 250 frames of the game
+		auto counter{ 0 };
+		while (GameEngine->refresh())
 		{
-			auto GameEngine = bootstrapEmptyEngine("TestScript");
-
-			auto ResourceManager = AnnGetResourceManager();
-			ResourceManager->addFileLocation("./unitTestScripts");
-			ResourceManager->initResources();
-
-			auto GameObjectManager = AnnGetGameObjectManager();
-			auto sun = GameObjectManager->createLightObject();
-			sun->setType(AnnLightObject::ANN_LIGHT_DIRECTIONAL);
-			sun->setDirection({ 0, -5, -1 });
-			sun->setPower(97);
-			auto floor = GameObjectManager->createGameObject("floorplane.mesh", "Floor");
-			auto ogre = GameObjectManager->createGameObject("Sinbad.mesh", "Ogre");
-
-			//Attach the behavior that cause to move the object by +0.02f on +y every frame
-			ogre->attachScript("GoUpBehavior");
-
-			//run 250 frames of the game
-			auto counter{ 0 };
-			while (GameEngine->refresh())
-			{
-				if (counter++ > 250) break;
-			}
-
-			//Check if script ran correctly
-			REQUIRE(ogre->getPosition().y >= 5);
+			if (counter++ > 250) break;
 		}
+
+		//Check if script ran correctly
+		REQUIRE(ogre->getPosition().y >= 5);
 	}
 
 	TEST_CASE("Object manipulation via scripting")
