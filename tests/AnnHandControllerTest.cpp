ecf50bc89de27a9afec146a3f080594cd1e4d7e1
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-11-12 02:08:10 +0100

    Add AnnHandControllerTest

---
+++ tests/AnnHandControllerTest.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+#include "engineBootstrap.hpp"
+
+namespace Annwvyn
+{
+	TEST_CASE("Set HandModel")
+	{
+		auto GameEngine = bootstrapTestEngine("HandModelTest");
+		auto smgr = AnnGetEngine()->getSceneManager();
+	}
+}
