839336a854856ac59b2f4eae583eec584a2c80af
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-11-24 23:34:12 +0100

    minor edit on the pch generation

--- pch/stdafx.h
+++ pch/stdafx.h
@@ -44,8 +44,6 @@
 #include <OgreEntity.h>
 #include <OgreLight.h>
 #include <OgreMatrix3.h>
-#include <Overlay/OgreFont.h>
-#include <Overlay/OgreFontManager.h>
 #include <OgreMaterialManager.h>
 #include <OgreMaterial.h>
 #include <OgreTechnique.h>
@@ -56,6 +54,8 @@
 #include <OgreMesh2.h>
 #include <OgreMeshManager.h>
 #include <OgreMeshManager2.h>
+#include <Overlay/OgreFont.h>
+#include <Overlay/OgreFontManager.h>
 #include <Compositor/OgreCompositorManager2.h>
 #include <Compositor/OgreCompositorWorkspaceDef.h>
 #include <Compositor/OgreCompositorWorkspace.h>
@@ -74,7 +74,6 @@
 #define BTOGRE_NO_VERBOSE_CHECK
 #include <BtOgre.hpp>
 #include <btBulletCollisionCommon.h>
-#include <BulletCollision/CollisionDispatch/btGhostObject.h>
 #include <btBulletDynamicsCommon.h>
 #include <LinearMath/btQuaternion.h>
 #include <LinearMath/btVector3.h>
@@ -89,22 +88,20 @@
 //TinyXML2
 #include <tinyxml2.h>
 
-#ifdef _WIN32
 //OpenVR (Valve/HTC Vive) API
 #include <openvr.h>
 #include <openvr_capi.h>
-#include <OVR_CAPI.h>
+
+#ifdef _WIN32
 //Oculus VR API
+#include <OVR_CAPI.h>
 #include <OVR_CAPI_GL.h>
 #include <Extras/OVR_Math.h>
-
 //Win32 API
 #include <windows.h>
 #include <io.h>
 #include <fcntl.h>
 #endif
-
-//OpenGL stuff :
 
 //GL Extension Wrangler
 #include <GL/glew.h>
