613c1750311a2a7f1586de7445f456c83c2516e9
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-12-11 15:00:44 +0100

    Add test that create a 3D text plane
    Signed-off by: Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>

---
+++ tests/Ann3DTextPlaneTest.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "engineBootstrap.hpp"
+
+namespace Annwvyn
+{
+	TEST_CASE("Create 3D text plane")
+	{
+		auto GameEngine = bootstrapTestEngine("Test3DTextPlane");
+
+		auto textPlane = std::make_shared<Ann3DTextPlane>(2, 1.5, "This is a text plane", 200,50,"SomeFont");
+		textPlane->setBackgroundColor(AnnColor{1,1,1,1});
+		textPlane->setPosition(AnnVect3{0,1.5,8});
+		textPlane->update();
+		
+		AnnGetVRRenderer()->_resetOgreTimer();
+		while(GameEngine->getTimeFromStartupSeconds() < 2)
+		{
+			GameEngine->refresh();
+		}
+
+	}
+}
