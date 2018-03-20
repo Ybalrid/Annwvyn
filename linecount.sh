15d3b97879b7214afb39806ba2c97ea8ea95aaf2
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2018-03-20 08:07:06 +0100

    updated linecount script
    Signed-off-by: Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>

--- linecount.sh
+++ linecount.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat src/* include/* example/*.chai example/*.cpp example/*.hpp template/*.cpp template/*.hpp template/*.chai tests/*.cpp test/*.hpp 2>/dev/null | wc -l
+cat src/* include/* renderer/*.txt renderer/Oculus/* renderer/OpenVR/* example/*.chai example/*.cpp example/*.hpp template/*.cpp template/*.hpp template/*.chai tests/*.cpp test/*.hpp 2>/dev/null | wc -l
