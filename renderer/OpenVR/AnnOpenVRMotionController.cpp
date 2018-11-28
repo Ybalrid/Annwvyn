50cb5215d81c953469f2df0b998a2e18b4df56dc
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2018-11-28 01:48:48 +0000

    Removed useless #pragma once in cpp file

--- renderer/OpenVR/AnnOpenVRMotionController.cpp
+++ renderer/OpenVR/AnnOpenVRMotionController.cpp
@@ -1,6 +1,5 @@
 // This is an open source non-commercial project. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-#pragma once
 
 #include "AnnOpenVRMotionController.hpp"
 #include <AnnGetter.hpp>
